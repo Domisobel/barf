4d09e9a60949dc842ba1d07b265e8916e5559dcc
Domisobel <93342577+Domisobel@users.noreply.github.com>
2023-06-11 17:44:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict'
+//kalk od wapna
+const btn = document.querySelector('.btn');
+const clear = document.querySelector('.clear');
+
+const modal = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.show-help');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+
+const result = document.querySelector('.result');
+const errorWeight = document.querySelector('.error-weight');
+const errorPortion = document.querySelector('.error-portion');
+
+//oblicz
+btn.addEventListener('click', function () {
+    const weight = document.querySelector('.weight').value;
+    const portion = document.querySelector('.portion').value;
+    const dawkowanie = weight * portion / 1000;
+    if (weight <= 0 || isNaN(weight)) {
+        errorWeight.textContent = "Nieprawidłowa waga posiłku";
+    }
+    if (portion <= 0 || isNaN(portion)) {
+        errorPortion.textContent = "Nieprawidłowa waga suplementu";
+    }
+    if (portion > 0 && weight > 0) {
+        result.textContent = `Dawka węglanu wapnia to ${dawkowanie}g.`;
+    }
+})
+//wyczyść
+clear.addEventListener('click', function () {
+    const weight = document.querySelector('.weight').value;
+    const portion = document.querySelector('.portion').value;
+    weight.value = '';
+    portion.value = '';
+    result.textContent = '';
+    errorPortion.textContent = '';
+    errorWeight.textContent = '';
+})
+//otwieranie
+btnOpenModal.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+})
+//zamykanie
+btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
+
